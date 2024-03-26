ad85af6e7113248d997533093090668701e2971e
Indalecio López <150592772+IndalecioHAB@users.noreply.github.com>
2024-03-26 23:29:52 +0100

    Update insertVideoModel.js

--- server/src/models/entries/insertVideoModel.js
+++ server/src/models/entries/insertVideoModel.js
@@ -2,13 +2,13 @@
 import getPool from '../../db/getPool.js';
 
 // Función que realiza una consulta a la base de datos para agregar una foto a una entrada.
-const insertVideoModel = async (videoName, entryId) => {
+const insertVideoModel = async (videoName, entryId, playerId) => {
     const pool = await getPool();
 
     // Insertamos la foto.
     const [video] = await pool.query(
-        `INSERT INTO entryVideos (videoName, entryId) VALUES (?, ?)`,
-        [videoName, entryId],
+        `INSERT INTO entryVideos (videoName, entryId, playerId) VALUES (?, ?, ?)`,
+        [videoName, entryId, playerId],
     );
 
     // Retornamos el ID que la base de datos ha asignado a la foto.
