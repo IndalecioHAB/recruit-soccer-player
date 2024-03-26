e1cd1ee5ff6c98482b981659ec3a49c29e26697f
Indalecio López <150592772+IndalecioHAB@users.noreply.github.com>
2024-03-27 00:42:33 +0100

    Update selectContractByIdModel.js

--- server/src/models/recruitmentRequests/selectContractByIdModel.js
+++ server/src/models/recruitmentRequests/selectContractByIdModel.js
@@ -19,7 +19,7 @@ const selectContractByIdModel = async (userId = '') => {
                 p.name,
                 u.username AS recruiter,
                 r.status
-            FROM recruitmentrequests r   
+            FROM recruitmentRequests r   
             INNER JOIN players p ON r.playerId = p.id  
             iNNER JOIN users u ON p.userId = u.id       
             WHERE r.familyId = ?
